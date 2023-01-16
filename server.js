b12378148728e2881d0c819fc6a7235295670686
Deshitha Thilindra (ddthilindra) <59472990+ddthilindra@users.noreply.github.com>
2023-01-16 11:00:57 +0530

    HTTP Status Codes

--- server.js
+++ server.js
@@ -6,18 +6,41 @@ const todos = [
   { id: 3, text: 'Todo three' },
 ];
 const server = http.createServer((req, res) => {
-  //   res.setHeader('Content-Type', 'text/html');
-  //   res.write('<h1>Hello</h1>');
-  //   res.write('<h2>Hello again</h2>');
-  //   res.end();
+  /*
+    
+    1.xx INFORMATION
 
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('X-Powerd-By', 'Node.js');
+    2.xx SUCCESS
+    200     | Success
+    201     | Created
+    204     | No Content
+    
+    3.xx REDIRECTION
+    304     | Not Modified
+    
+    4.xx CLIENT ERROR
+    400     | Bad Request
+    401     | Unauthorized
+    404     | Not Found
+
+    5.xx SERVER ERROR
+    500     | Internal Server Error
+
+  */
+
+  //   res.statusCode = 404;
+  //   res.setHeader('Content-Type', 'application/json');
+  //   res.setHeader('X-Powerd-By', 'Node.js');
+
+  res.writeHead(404, {
+    'Content-Type': 'application/json',
+    'X-Powerd-By': 'Node.js',
+  });
 
   res.end(
     JSON.stringify({
-        success:true,
-        data:todos
+      success: true,
+      data: todos,
     })
   );
 });
