f88412bab12431572448fa4b31571e4d3903c7df
Deshitha Thilindra (ddthilindra) <59472990+ddthilindra@users.noreply.github.com>
2023-01-16 14:24:42 +0530

    Logger middleware added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const indexRoutes= require('./routes/index')
+const logger=require('./middleware/logger')
+const morgan = require('morgan')
 
 // Load env
 dotenv.config();
@@ -11,6 +13,12 @@ app.get('/',(req,res)=>{
     res.send('API is running..!')
 })
 
+// app.use(logger)
+if(process.env.NODE_ENV == 'development'){
+    app.use(morgan('dev'))
+}
+
+// Mount routers
 app.use('/',indexRoutes)
 
 const PORT = process.env.PORT || 5000;
