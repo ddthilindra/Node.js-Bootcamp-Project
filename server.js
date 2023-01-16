931e352bc0ce5de9b59975d5d193ae76123e6e3f
Deshitha Thilindra (ddthilindra) <59472990+ddthilindra@users.noreply.github.com>
2023-01-16 11:19:32 +0530

    Sending data to server

--- server.js
+++ server.js
@@ -6,36 +6,21 @@ const todos = [
   { id: 3, text: 'Todo three' },
 ];
 const server = http.createServer((req, res) => {
-  /*
-    
-    1.xx INFORMATION
-
-    2.xx SUCCESS
-    200     | Success
-    201     | Created
-    204     | No Content
-    
-    3.xx REDIRECTION
-    304     | Not Modified
-    
-    4.xx CLIENT ERROR
-    400     | Bad Request
-    401     | Unauthorized
-    404     | Not Found
-
-    5.xx SERVER ERROR
-    500     | Internal Server Error
-
-  */
-
-  //   res.statusCode = 404;
-  //   res.setHeader('Content-Type', 'application/json');
-  //   res.setHeader('X-Powerd-By', 'Node.js');
-
   res.writeHead(200, {
     'Content-Type': 'application/json',
     'X-Powerd-By': 'Node.js',
   });
+
+  let body = [];
+
+  req
+    .on('data', (chunk) => {
+      body.push(chunk);
+    })
+    .on('end', () => {
+      body = Buffer.concat(body).toString();
+      console.log(body);
+    });
 
   res.end(
     JSON.stringify({
