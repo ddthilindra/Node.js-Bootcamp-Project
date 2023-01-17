cd18bb315b7ece806877834039d429469ed8aa6c
Deshitha Thilindra (ddthilindra) <59472990+ddthilindra@users.noreply.github.com>
2023-01-17 19:50:17 +0530

    Database seeder for bootcamp

---
+++ seeder.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config();
+
+// Load models
+const Bootcamp = require('./models/bootcamps');
+// const Course = require('./models/Course');
+// const User = require('./models/User');
+// const Review = require('./models/Review');
+
+// Read JSON files
+const bootcamps = require('./_data/bootcamps.json');
+// const courses = require('./_data/courses.json');
+// const users = require('./_data/users.json');
+// const reviews = require('./_data/reviews.json');
+
+// Connect to DB
+connectDB();
+
+// Script : npm run data:import
+const importData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    // await Course.deleteMany();
+    // await User.deleteMany();
+    // await Review.deleteMany();
+
+    await Bootcamp.insertMany(bootcamps);
+
+    console.log('Data Imported!');
+    process.exit();
+  } catch (error) {
+    console.log(`${error}`);
+    process.exit(1);
+  }
+};
+
+// Script : npm run data:destroy
+const destroyData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    // await Course.deleteMany();
+    // await User.deleteMany();
+    // await Review.deleteMany();
+
+    console.log('Data Destroyed!');
+    process.exit();
+  } catch (error) {
+    console.log(`${error}`);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === '-d') {
+  destroyData();
+} else {
+  importData();
+}
