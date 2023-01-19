65daaf837109256d3559fe91899a0a9ae6a4f02a
Deshitha Thilindra (ddthilindra) <59472990+ddthilindra@users.noreply.github.com>
2023-01-20 00:49:40 +0530

    Single Course and Add Course

--- controllers/course.js
+++ controllers/course.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Course = require('../models/course');
+const Bootcamp = require('../models/bootcamps');
 
 // @desc    Get all courses
 // @route   GET /courses
@@ -16,11 +17,11 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     // Get All Courses
     // Get All Courses with get all bootcamp
     // query = Course.find().populate('bootcamp');
-    
+
     // select specific fields
     query = Course.find().populate({
-        path:'bootcamp',
-        select:'name description'
+      path: 'bootcamp',
+      select: 'name description',
     });
   }
 
@@ -30,5 +31,45 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     success: true,
     count: courses.length,
     data: courses,
+  });
+});
+
+// @desc    Get single course
+// @route   GET /courses/:id
+// @acess   Public
+exports.getCourseByID = asyncHandler(async (req, res, next) => {
+  
+    const course = await Course.findById(req.params.id).populate({
+    path: 'bootcamp',
+    select: 'name, description',
+  });
+
+  if(!course){
+    return next(new ErrorResponse(`No course with the id of ${req.params.id}`),404)
+  }
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+// @desc    Add course
+// @route   POST /bootcamp/:bootcampId/courses
+// @acess   Private
+exports.addCourse = asyncHandler(async (req, res, next) => {
+  
+    req.body.bootcamp =req.params.bootcampId
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId)
+
+  if(!bootcamp){
+    return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`),404)
+  }
+
+  const course = await Course.create(req.body)
+
+  res.status(200).json({
+    success: true,
+    data: course,
   });
 });
--- routes/courseRoute/course.js
+++ routes/courseRoute/course.js
@@ -3,5 +3,8 @@ const courseController = require('../../controllers/course')
 
 // /bootcamps/:bootcampId/courses continue with here
 router.get('/',courseController.getCourses)
+router.post('/',courseController.addCourse)
+
+router.get('/:id',courseController.getCourseByID)
 
 module.exports = router
