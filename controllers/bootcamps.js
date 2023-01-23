f8e6ead3d08af55aceeae44e640085e85173c78d
Deshitha Thilindra (ddthilindra) <59472990+ddthilindra@users.noreply.github.com>
2023-01-23 16:41:19 +0530

    Bootcamp Ownership

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -60,16 +60,26 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 // @route   PUT /bootcamps/:id
 // @acess   Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
+  let bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
+
+  // Make sure user is bootcamp owner
+  if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin'){
+    return next(
+      new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`, 401)
+    );
+  }
+
+  bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
   res.status(200).json({
     success: true,
     msg: `Update bootcamp ${req.params.id}`,
@@ -81,7 +91,6 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route   DELETE /bootcamps/:id
 // @acess   Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  // const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
   // find the bootcamp
   const bootcamp = await Bootcamp.findById(req.params.id);
@@ -89,6 +98,13 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user is bootcamp owner
+  if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin'){
+    return next(
+      new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`, 401)
     );
   }
 
