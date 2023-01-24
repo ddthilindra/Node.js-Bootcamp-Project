d23581cb1d4050ee25a0e5acfb2ce5de6faacd66
Deshitha Thilindra (ddthilindra) <59472990+ddthilindra@users.noreply.github.com>
2023-01-24 23:00:57 +0530

    Add Review For Bootcamp

--- controllers/review.js
+++ controllers/review.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Review = require('../models/review');
+const Bootcamp = require('../models/bootcamps');
 
 // @desc    Get all reviews
 // @route   GET /reviews
@@ -26,17 +27,40 @@ exports.getReviews = asyncHandler(async (req, res, next) => {
 // @route   GET /review/:id
 // @acess   Public
 exports.getReview = asyncHandler(async (req, res, next) => {
-    const review = await Review.findById(req.params.id)
-    
+  const review = await Review.findById(req.params.id);
+
   if (!review) {
     return next(
-      new ErrorResponse(`No review found with the id of ${req.params.id}`,404),
+      new ErrorResponse(`No review found with the id of ${req.params.id}`, 404)
     );
   }
 
   res.status(200).json({
     success: true,
     count: review.length,
+    data: review,
+  });
+});
+
+// @desc    Add review
+// @route   POST bootcamps/:bootcampId/reviews
+// @acess   Private
+exports.addReview = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`No review found with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  const review = await Review.create(req.body);
+
+  res.status(201).json({
+    success: true,
     data: review,
   });
 });
--- routes/reviewRoute/review.js
+++ routes/reviewRoute/review.js
@@ -8,9 +8,8 @@ const advancedResults = require('../../middleware/advancedResult')
 
 // /bootcamps/:bootcampId/reviews continue with here
 router.get('/',advancedResults(Review,{path:'bootcamp',select:'name description'}),reviewController.getReviews)
-// router.post('/', authMiddleware.protect, authMiddleware.authorize('publisher','admin'), reviewController.addreview)
-
 router.get('/:id',reviewController.getReview)
+router.post('/', authMiddleware.protect, authMiddleware.authorize('user','admin'), reviewController.addReview)
 // router.put('/:id', authMiddleware.protect, authMiddleware.authorize('publisher','admin'), reviewController.updatereview)
 // router.delete('/:id', authMiddleware.protect, authMiddleware.authorize('publisher','admin'), reviewController.deletereview)
 
