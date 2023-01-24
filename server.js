5cc0e16374efc6367d8f88beaaacbabc626a9790
Deshitha Thilindra (ddthilindra) <59472990+ddthilindra@users.noreply.github.com>
2023-01-25 00:40:18 +0530

    XSS Protection & Security Headers

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const connectDB = require('./config/db');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 
 // Load env vars
 dotenv.config();
@@ -36,6 +38,12 @@ app.use(fileupload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security Headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
