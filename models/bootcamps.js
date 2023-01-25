fa53bddbed99c1a0a2f37e1a314ff6d830c8126d
Deshitha Thilindra (ddthilindra) <59472990+ddthilindra@users.noreply.github.com>
2023-01-25 13:57:12 +0530

    Geocoder Hook - MapQuest API Updated

--- models/bootcamps.js
+++ models/bootcamps.js
@@ -118,25 +118,24 @@ BootcampSchema.pre('save', function (next) {
   next();
 });
 
-// // Geocode & create location field
-// BootcampSchema.pre('save', async function (next) {
-//   const loc = await geocoder.geocode(this.address);
-//   this.location = {
-//     type: 'Point',
-//     coordinates: [loc[0].longitude, loc[0].latitude],
-//     formattedAddress: loc[0].formattedAddress,
-//     street: loc[0].streetName,
-//     city: loc[0].city,
-//     state: loc[0].state,
-//     zipcode: loc[0].zipcode,
-//     country: loc[0].countryCode,
-//   };
+// Geocode & create location field
+BootcampSchema.pre('save', async function(next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode
+  };
 
-//   // Do not save address in DB
-//   this.address = undefined;
-
-//   next();
-// });
+  // Do not save address in DB
+  this.address = undefined;
+  next();
+});
 
 // Reverse populate with virtuals
 BootcampSchema.virtual('courses', {
